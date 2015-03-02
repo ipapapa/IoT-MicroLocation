6e4efafdf502f9b20fafba60faf87c9e6f05d7a3
ipapapan <ioannis@macosx>
2015-03-02 08:03:24 -0500

    updated the copyright

--- micro-location/micro-locationTests/micro_locationTests.m
+++ micro-location/micro-locationTests/micro_locationTests.m
@@ -3,7 +3,7 @@
 //  micro-locationTests
 //
 //  Created by Ian Hamilton on 2/9/15.
-//  Copyright (c) 2015 Lost Eon Entertainment. All rights reserved.
+//  Copyright (c) 2015 Purdue University. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
