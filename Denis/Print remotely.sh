d2c1ea1c3536e4cd539b7a73cfd7754e78945cd4
dbershev <dbershev@purdue.edu>
2015-04-28 19:11:23 -0400

    changed titles

--- Denis/Print remotely.sh
+++
@@ -1,8 +0,0 @@
-#Uses passwordless ssh via rsa key.
-#!/bin/bash
-USERNAME=denis
-HOSTS="10.10.1.18"
-SCRIPT="sudo lp -o test.txt"
-for HOSTNAME in ${HOSTS} ; do
-    ssh -o StrictHostKeyChecking=no -l ${USERNAME} ${HOSTNAME} -p 1220 "${SCRIPT}"
-done
