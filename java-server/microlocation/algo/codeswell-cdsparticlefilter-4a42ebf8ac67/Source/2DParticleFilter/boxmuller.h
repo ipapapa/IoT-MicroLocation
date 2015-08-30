7645e65b3fd86dc72e22a04de9b8cc101638d055
faheem0 <faheem0@purdue.edu>
2015-08-30 02:14:06 -0400

    unrefined changes

--- java-server/microlocation/algo/codeswell-cdsparticlefilter-4a42ebf8ac67/Source/2DParticleFilter/boxmuller.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef iBeaconParticleFilter_boxmuller_h //#ifndef is often used in C (versus Objective-C) to prevent header files from being included more than once during compilation (something that can be challenging in a large project with many source files)
-
-#define iBeaconParticleFilter_boxmuller_h
-
-double box_muller(double m, double s);
-
-#endif
