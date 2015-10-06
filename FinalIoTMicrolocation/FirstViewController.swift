e92e496be81f70f173784b285acb2f181c09e9f9
Ibrahim Darwish <iadarwis@ncsu.edu>
2015-10-06 03:16:46 -0400

    Initial Commit

---
+++ FinalIoTMicrolocation/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FinalIoTMicrolocation
+//
+//  Created by Ibrahim Darwish on 10/6/15.
+//  Copyright © 2015 NCSU. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
