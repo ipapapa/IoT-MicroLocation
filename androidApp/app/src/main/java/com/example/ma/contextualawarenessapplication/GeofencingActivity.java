e200edfa01a0a8e020e0853611ad8119dec87074
MILKSHAK <maxinghua123000@gmail.com>
2016-04-25 16:11:34 -0400

    first semester demo version

--- androidApp/app/src/main/java/com/example/ma/contextualawarenessapplication/GeofencingActivity.java
+++ androidApp/app/src/main/java/com/example/ma/contextualawarenessapplication/GeofencingActivity.java
@@ -1,96 +1,47 @@
 package com.example.ma.contextualawarenessapplication;
 
-import android.Manifest;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.pm.PackageManager;
-import android.os.Build;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import android.app.Activity;
-import android.os.RemoteException;
 import android.widget.TextView;
 
-import org.altbeacon.beacon.Beacon;
-import org.altbeacon.beacon.BeaconConsumer;
-import org.altbeacon.beacon.BeaconManager;
-import org.altbeacon.beacon.BeaconParser;
-import org.altbeacon.beacon.RangeNotifier;
-import org.altbeacon.beacon.Region;
 
-import java.text.SimpleDateFormat;
-import java.util.Collection;
-import java.util.Date;
-import java.util.Locale;
 
-public class GeofencingActivity extends AppCompatActivity implements BeaconConsumer {
+public class GeofencingActivity extends AppCompatActivity{
     private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
     protected static final String TAG = "GeofencingActivity";
-    private BeaconManager beaconManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geofencing);
 
-        beaconManager = BeaconManager.getInstanceForApplication(this);
-        beaconManager.getBeaconParsers().add(new BeaconParser().
-                setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
-        beaconManager.bind(this);
+        // Show the Up button in the action bar.
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        beaconManager.unbind(this);
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (beaconManager.isBound(this)) beaconManager.setBackgroundMode(true);
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (beaconManager.isBound(this)) beaconManager.setBackgroundMode(false);
-    }
-
-    @Override
-    public void onBeaconServiceConnect() {
-        beaconManager.setRangeNotifier(new RangeNotifier() {
-            @Override
-            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
-                for (Beacon beacon : beacons) {
-                    logToDisplay(getCurrentTimeStamp() + " | Beacon " + beacon.toString() + " is about " + beacon.getDistance() + " meters away.");
-                }
-            /*if (beacons.size() > 0) {
-            	EditText editText = (EditText)GeofencingActivity.this
-						.findViewById(R.id.geofencingText);
-                Beacon firstBeacon = beacons.iterator().next();
-                logToDisplay("The first beacon "+firstBeacon.toString()+" is about "+firstBeacon.getDistance()+" meters away.");            }
-        */}
-
-        });
-
-        try {
-            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
-        } catch (RemoteException e) {   }
-    }
-    private void logToDisplay(final String line) {
-        runOnUiThread(new Runnable() {
-            public void run() {
-                TextView editText = (TextView)GeofencingActivity.this
-                        .findViewById(R.id.geofencingText);
-                editText.append(line+"\n");
-            }
-        });
-    }
-
-    private static String getCurrentTimeStamp() {
-        Locale locale = new Locale("es", "ES");
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss.SSS", locale);
-        Date now = new Date();
-        return sdf.format(now);
+        //===================================================
+        //setContentView(R.layout.detail_log);
+        LocalBroadcastManager.getInstance(this).registerReceiver(
+                new BroadcastReceiver() {
+                    @Override
+                    public void onReceive(Context context, Intent intent) {
+                        String logMessage = intent.getStringExtra(BackgroundService.LOG_MESSAGE);
+                        //ViewGroup viewGroup = (ViewGroup) ((ViewGroup) findViewById(android.R.id.content)).getChildAt(0);
+                        //LayoutInflater inflater = getLayoutInflater();
+                        //TextView textView = (TextView) inflater.inflate(R.layout.detail_log, viewGroup, false);
+                        TextView textView = (TextView)findViewById(R.id.geofencingText);
+                        if(textView != null) textView.append("\n" + logMessage);
+                    }
+                }, new IntentFilter(BackgroundService.ACTION_BROADCAST)
+        );
     }
 }
